e03ab1d10c7a209d3c7836da0fc86a5fcfc229e9
tushar_singh <tscorpo2@gmail.com>
2025-01-06 19:39:35 +0530

    created account and transfer pages

---
+++ src/components/settings.tsx
@@ -0,0 +1,24 @@
+
+
+export default function Settings(){
+    return <div className="flex flex-col">
+            <div className="bg-white flex flex-col rounded-lg my-2 p-2">
+                <div>Personal Info</div>
+                <div>Personal Info</div>
+                <div>Personal Info</div>
+                <div>Personal Info</div>
+                <div>Personal Info</div>
+                <div>Personal Info</div>
+            </div>
+            <div className="bg-white flex flex-col rounded-lg my-2 p-2">
+                <div>Preferences</div>
+                <div>Preferences</div>
+                <div>Preferences</div>
+                <div>Preferences</div>
+                <div>Preferences</div>
+                <div>Preferences</div>
+            </div>
+            <div>
+        </div>
+    </div>
+}
